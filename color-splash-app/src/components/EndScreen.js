5da1797147ab5458f882a96659cd0957bb146aeb
James <jaames.chen@gmail.com>
2021-05-07 12:56:23 -0400

    bare bones hiscore and endscreen components

--- color-splash-app/src/components/EndScreen.js
+++ color-splash-app/src/components/EndScreen.js
@@ -5,6 +5,7 @@ class EndScreen extends Component {
     return(
       <div>
         <h1>EndScreen</h1>
+        <h2 onClick={()=>console.log('Bring me to home page')}>Play Again?</h2>
       </div>
     )
   }
